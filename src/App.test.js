a167b40139c0c1950cf38e9a292eda3dfd45850d
unah254 <greisunah@gmail.com>
2021-03-31 13:34:27 +0300

    add-test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,17 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testing sum', () => {
+  function sum(a, b) {
+     return a + b;
+  }
+
+  it('should equal 4',()=>{
+     expect(sum(2,2)).toBe(4);
+    })
+
+  test('also should equal 4', () => {
+      expect(sum(2,2)).toBe(4);
+    }) 
 });
+
